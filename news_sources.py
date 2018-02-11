501edd688dc5c335da3206e768bf964cf22fc40e
gn01842919 <gn01842919@gmail.com>
2018-02-11 15:25:38 +0800

    Can parse news entries for Google and Yahoo

--- news_sources.py
+++ news_sources.py
@@ -1,12 +1,20 @@
+"""
+have to do:
+    pip install feedparser python-dateutil beautifulsoup4
+"""
 import feedparser
 from dateutil import parser as date_parser
 from datetime import datetime
+from collections import namedtuple
+from bs4 import BeautifulSoup
 
 """
 To-Do:
     1. write unit tests
     2. Yahoo stock rss
 """
+
+NewsEntry = namedtuple("NewsEntry", "title description link published_time")
 
 
 class MyFeed:
@@ -16,7 +24,7 @@ class MyFeed:
         self.link = link
         self.language = language
         self.published_time = published_time
-        self.entries = entries  # Reference to a list object
+        self.entries = entries  # Reference to a list of named_tuples
 
     def __str__(self):
         return (
@@ -26,12 +34,12 @@ class MyFeed:
             "[Link]     : {}\n"
             "[Language] : {}\n"
             "[Published]: {}\n"
-            # "[Entries]: {}"
+            "[Entries #]: {}\n"
             "++++++++++++++++++++\n"
             .format(
                 self.title, self.subtitle, self.link,
                 self.language, self.published_time,
-                # self.entries
+                len(self.entries)
             )
         )
 
@@ -41,29 +49,43 @@ class RSSFeedParser:
     @classmethod
     def parse_feed(cls, url):
         feed = feedparser.parse(url)
-        title = cls._get_title_from_feed(feed)
-        subtitle = cls._get_subtitle_from_feed(feed)
-        link = cls._get_link_from_feed(feed)
-        language = cls._get_language_from_feed(feed)
-        published_time = cls._get_time_from_feed(feed)
-        entries = cls._get_entries_from_feed(feed)
+
+        title = cls._get_title_from_feed(feed.feed)
+        subtitle = cls._get_subtitle_from_feed(feed.feed)
+        link = cls._get_link_from_feed(feed.feed)
+        language = cls._get_language_from_feed(feed.feed)
+        published_time = cls._get_time_from_feed(feed.feed)
+
+        entries = list(cls._get_entries_from_feed(feed.entries))
         return MyFeed(title, subtitle, link, language, published_time, entries)
+
+    @classmethod
+    def _get_entries_from_feed(cls, entries):
+        for entry in entries:
+            title = cls._get_title_from_feed(entry)
+            description = cls._get_subtitle_from_feed(entry)
+            link = cls._get_link_from_feed(entry)
+            published_time = cls._get_time_from_feed(entry)
+            yield NewsEntry(title, description, link, published_time)
 
     @staticmethod
     def _get_title_from_feed(feed):
-        return feed.feed.title
+        return feed.title
 
     @staticmethod
     def _get_subtitle_from_feed(feed):
-        return feed.feed.subtitle
+        try:
+            return feed.subtitle.strip()
+        except AttributeError:
+            return feed.description.strip()
 
     @staticmethod
     def _get_link_from_feed(feed):
-        return feed.feed.link
+        return feed.link
 
     @staticmethod
     def _get_language_from_feed(feed):
-        return feed.feed.language
+        return feed.language
 
     @staticmethod
     def _get_time_from_feed(feed):
@@ -74,20 +96,44 @@ class RSSFeedParser:
         #     https://stackoverflow.com/questions/20867795/python-how-to-get-timezone-from-rss-feed
 
         try:
-            published_time = date_parser.parse(feed.feed.published)
+            published_time = date_parser.parse(feed.published)
         except AttributeError:
-            # feed.feed.published is not provided
+            # feed.published is not provided
             published_time = datetime.utcnow()
 
         return published_time
 
-    @staticmethod
-    def _get_entries_from_feed(feed):
-        return feed.entries  # should be more complicated
-
 
 class GoogleFeedParser(RSSFeedParser):
-    pass
+
+    @staticmethod
+    def _get_subtitle_from_feed(feed):
+
+        try:
+            return feed.subtitle
+        except AttributeError:
+            # feed.description is a list of same news from different sources
+            # So let's parse it
+
+            output = ""
+
+            bsobj = BeautifulSoup(feed.description, "html.parser")
+            news_sources_li = bsobj.findAll("li")
+            # <li>
+            #     <a href="http://www.cna.com.tw/news/aopl/201802110062-1.aspx" target="_blank">
+            #          以色列攻擊敘利亞境內伊朗目標美力挺
+            #     </a>
+            #     &nbsp;&nbsp;
+            #     <font color="#6F6F6F">
+            #         中央社即時新聞
+            #     </font>
+            # </li>
+            for src in news_sources_li:
+                news_title = src.a.get_text()
+                news_source = src.font.get_text()
+                output += "[{}] {}\n".format(news_source, news_title)
+
+            return output.strip()
 
 
 class YahooFeedParser(RSSFeedParser):
@@ -149,5 +195,25 @@ if __name__ == '__main__':
     google_news = GoogleNews()
     print(google_news.get_feed_object('WORLD'))
 
+    entries = google_news.get_feed_object('WORLD').entries
+    for i in range(2):
+        entry = entries[i]
+        print('-' * 10)
+        print(entry.title)
+        print(entry.description)
+        print(entry.link)
+        print(entry.published_time)
+        print('-' * 10)
+
     yahoo_news = YahooNews()
     print(yahoo_news.get_feed_object('politics'))
+
+    entries = yahoo_news.get_feed_object('politics').entries
+    for i in range(2):
+        entry = entries[i]
+        print('-' * 10)
+        print(entry.title)
+        print(entry.description)
+        print(entry.link)
+        print(entry.published_time)
+        print('-' * 10)
