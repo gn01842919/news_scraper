7ec9e6ca879e6b980e47b1bbbf139d28bbc32fa2
gn01842919 <gn01842919@gmail.com>
2018-03-07 18:20:00 +0800

    Add reset_db_data.py to remove all news and rules in DB

---
+++ reset_db_data.py
@@ -0,0 +1,8 @@
+from db_news_api import NewsDatabaseAPI
+from db_operation_api.mydb import PostgreSqlDB
+
+
+if __name__ == '__main__':
+    with PostgreSqlDB(database="my_focus_news") as conn:
+        db_api = NewsDatabaseAPI(conn, table_prefix="shownews_")
+        db_api.remove_scraping_rules_and_relations()
